e002894de3723c2c7b64dbc8dee8faf447ca9cd1
Ashley <teshay93@hotmail.com>
2021-03-17 20:41:08 -0400

    almost final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 /* ==== SHOW MENU ==== */
 const showMenu = (toggleId, navId) => {
-  const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId);
+	const toggle = document.getElementById(toggleId),
+		nav = document.getElementById(navId);
 
-  if (toggle && nav) {
-    toggle.addEventListener("click", () => {
-      nav.classList.toggle("show-menu");
-    });
-  }
+	if (toggle && nav) {
+		toggle.addEventListener("click", () => {
+			nav.classList.toggle("show-menu");
+		});
+	}
 };
 
 showMenu("nav-toggle", "nav-menu");
@@ -16,44 +16,68 @@ showMenu("nav-toggle", "nav-menu");
 const navLink = document.querySelectorAll(".nav_link");
 
 function linkAction() {
-  const navMenu = document.getElementById("nav-menu");
-  navMenu.classList.remove("show-menu");
+	const navMenu = document.getElementById("nav-menu");
+	navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener("click", linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /* ==== SCROLL SECTIONS ACTIVE LINK ==== */
-const section = document.querySelectorAll('section[id]')
+const section = document.querySelectorAll("section[id]");
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+	const scrollY = window.pageYOffset;
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop -  50
-        sectionId = current.getAttribute('id')
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 50;
+		sectionId = current.getAttribute("id");
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav_menu aa[href+=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document
-              .querySelector(".nav_menu aa[href+=" + sectionId + "]")
-              .classList.remove("active-link");
-        }
-    })
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document
+				.querySelector(".nav_menu aa[href+=" + sectionId + "]")
+				.classList.add("active-link");
+		} else {
+			document
+				.querySelector(".nav_menu aa[href+=" + sectionId + "]")
+				.classList.remove("active-link");
+		}
+	});
 }
-window.addEventListener('scroll' , scrollActive);
+window.addEventListener("scroll", scrollActive);
 
 //===== CHANGE BACKGROUND HEADER ===== //
-function scrollHeader(){
-    const header = document.getElementById('header')
-    if(this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove("scroll-header");
+function scrollHeader() {
+	const header = document.getElementById("header");
+	if (this.scrollY >= 200) header.classList.add("scroll-header");
+	else header.classList.remove("scroll-header");
 }
-window.addEventListener('scroll' , scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
 /* ===== SHOW SCROLL TOP ===== */
 function scrollTop() {
-  const scrollTop = document.getElementById("scroll-top");
-  if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
-  else header.classList.remove("show-scroll");
+	const scrollTop = document.getElementById("scroll-top");
+	if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
+	else header.classList.remove("show-scroll");
 }
 window.addEventListener("scroll", scrollTop);
+
+/* ==== MIX IT UP ==== */
+const mixer = mixitup(".portfolio_container", {
+	selectors: {
+		target: ".portfolio_content",
+	},
+	animation: {
+		duration: 400,
+	},
+});
+
+/* ==== active portfolio ==== */
+const linkPortfolio = document.querySelectorAll(".portfolio_item");
+
+function activePortfolio() {
+	if (linkPortfolio) {
+		linkPortfolio.forEach((l) => l.classList.remove("active-portfolio"));
+		this.classList.add("active-portfolio");
+	}
+}
+linkPortfolio.forEach((l) => l.addEventListener("click", activePortfolio));
